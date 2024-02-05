43ef6319037f12bbacf31ea54f6ef7afcd607184
dHurlburt <hurlbu19@msu.edu>
2024-02-05 10:20:06 -0500

    Added a lot of layout and footer changes

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("Phillip", function(firstName, lastName){
+    return `<div class="Phillip">
+      <div class="PhillipFirstName">${firstName}</div>
+      <div class="PhillipLastName">${lastName}</div>
+    </div>`;
+  });
 };
